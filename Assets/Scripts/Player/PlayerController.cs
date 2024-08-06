953d89fdbffd9412a1acded5710df1588c8f9f89
Deependra-Das <iamdeep75@gmail.com>
2024-08-07 02:55:47 +0530

    Updated Player Controller Script for Animation & Flip
    - Updated the Player controller script to play run animation and flip player according to the horizontal input

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -4,13 +4,32 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public Animator animator;
     private void Awake()
     {
         Debug.Log("Player Controller Awake.");
     }
 
-    private void OnCollisionEnter2D(Collision2D collision)
+    //private void OnCollisionEnter2D(Collision2D collision)
+    //{
+    //    Debug.Log("Collision: "+ collision.gameObject.name);
+    //}
+
+    private void Update()
     {
-        Debug.Log("Collision: "+ collision.gameObject.name);
+        float speed = Input.GetAxisRaw("Horizontal");
+        animator.SetFloat("Speed", Mathf.Abs(speed));
+
+        Vector3 scale = transform.localScale;
+        if (speed < 0f)
+        {
+            scale.x = -1f * Mathf.Abs(scale.x);
+        }
+        else
+        {
+            scale.x = Mathf.Abs(scale.x);
+        }
+        transform.localScale = scale;
+
     }
 }
