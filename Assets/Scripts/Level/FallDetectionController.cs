0de09176213a8b7934efbe56ec303e79b7b98655
Deependra-Das <iamdeep75@gmail.com>
2024-08-14 00:37:38 +0530

    Added Logic for Fall Detection in Levels

---
+++ Assets/Scripts/Level/FallDetectionController.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class FallDetectionController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+        private void OnTriggerEnter2D(Collider2D other)
+    {
+        // Debug.Log("Collision Detected");
+
+        if (other.gameObject.GetComponent<PlayerController>() != null)
+        {
+            Debug.Log("Player Fell Down");
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+    }
+}
