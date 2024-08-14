09a145895dbdc9a32a6d239b5fbd4b74bb0305c4
Deependra-Das <iamdeep75@gmail.com>
2024-08-14 05:45:29 +0530

    Added Key Spin Animation

--- Assets/Scripts/Collectibles/KeyController.cs
+++ Assets/Scripts/Collectibles/KeyController.cs
@@ -5,12 +5,22 @@ using UnityEngine.SceneManagement;
 
 public class KeyController : MonoBehaviour
 {
+    private Animator keyCollectibleAnimator;
+
+    private void Start()
+    {
+        keyCollectibleAnimator = GetComponent<Animator>();
+ 
+    }
+
     private void OnCollisionEnter2D(Collision2D other)
     {
         if (other.gameObject.GetComponent<PlayerController>() != null)
         {
             PlayerController playerControllerObject = other.gameObject.GetComponent<PlayerController>();
+            keyCollectibleAnimator.SetBool("KeyCollected", true);
             playerControllerObject.KeyPickedUp();
+
             Destroy(gameObject);
         }
     }
